8221bfe6b6fb1d11ad09b7736630fcfc1b4bdfa0
Nestor Navarro <nestornav@gmail.com>
2022-05-03 23:30:24 -0300

    double linked list

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -3,27 +3,15 @@ class Node:
         self.data = data
         self.next = None
 
-class LinkedList:
+class BaseList:
     def __init__(self):
         self.head = None
 
-    def push(self, data):
-        '''
-        Recive the data for the new node. Create the node and append it to the head of the list.
-        '''
-        # create the new node
-        node = Node(data)
-
-        if self.head != None:
-            node.next = self.head
-        self.head = node
+    def push(self):
+        pass
 
     def print_nodes(self):
-        tmp = self.head
-        if self.head is not None:
-            while tmp:
-                print(tmp.data)
-                tmp = tmp.next
+        pass
 
     def __len__(self):
         c = 0
@@ -38,11 +26,58 @@ class LinkedList:
 
             return c
 
+class LinkedList(BaseList):
+    def push(self, data):
+        '''
+        Recive the data for the new node. Create the node and append it to the head of the list.
+        '''
+        # create the new node
+        node = Node(data)
+
+        if self.head is not None:
+            node.next = self.head
+        self.head = node
+
+    def print_nodes(self):
+        tmp = self.head
+        if self.head is not None:
+            while tmp:
+                print(tmp.data)
+                tmp = tmp.next
+
+
 class DoublelinkedList(LinkedList):
     def push(self, data):
-        pass
+        node = Node(data)
+
+        if self.head is not None:
+           tmp = self.head
+           node.next = tmp
+           while  True:
+               if tmp.next == self.head:
+                    self.head = node
+                    tmp.next = node
+                    break
+               tmp = tmp.next
+        else:
+            self.head = node
+            self.head.next = node
     def print_nodes(self):
-        pass
+        tmp = self.head
+        while True:
+            print(tmp.data)
+            tmp = tmp.next
+            if tmp == self.head:
+                break
 
     def __len__(self):
-        pass
+        tmp = self.head
+        c = 0
+
+        while True:
+            c +=1
+            tmp = tmp.next
+            if tmp == self.head:
+                break
+
+        return c
